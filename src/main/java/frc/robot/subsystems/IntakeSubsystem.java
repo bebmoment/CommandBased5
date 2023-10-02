322911347381b4d61182a26402a298ae7d629d0d
beb <benwangott@gmail.com>
2023-10-02 18:40:34 -0400

    formatting

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,29 +1,37 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants.PneumaticsConstants;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class IntakeSubsystem extends SubsystemBase {
     
     private final CANSparkMax intake = new CANSparkMax(IntakeConstants.INTAKE_MOTOR_ID, MotorType.kBrushless);
 
-    private final DoubleSolenoid intakePneumatic = new DoubleSolenoid(PneumaticsConstants.PCM,
-      PneumaticsModuleType.CTREPCM, IntakeConstants.INTAKE_FORWARD_PORT, IntakeConstants.INTAKE_REVERSE_PORT);
+    private boolean stall;
     
     public IntakeSubsystem() {
 
     }
 
-    public void open() {
-        
+    public void setMotor(double speed) {
+        intake.set(speed);
+    }
+
+    public boolean getStall() {
+      return stall;
+    }
+
+    public void setStall(boolean idle) {
+      this.stall = idle;
+    }
+
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+      SmartDashboard.putNumber("Intake Speed", intake.get());
     }
 }
