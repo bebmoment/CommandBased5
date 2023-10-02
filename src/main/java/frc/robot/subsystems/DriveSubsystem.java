3623b2e97c34613ebcfd15b48479bb393b05ff98
beb <benwangott@gmail.com>
2023-10-02 09:10:58 -0400

    heehoo

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+// import com.kauailabs.navx.frc.AHRS;
+
+
+public class DriveSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+
+  // private final Timer timer = new Timer();
+
+  private final WPI_VictorSPX driveFrontLeft = new WPI_VictorSPX(DriveConstants.DRIVE_FRONT_LEFT);
+  private final WPI_TalonSRX driveFrontRight = new WPI_TalonSRX(DriveConstants.DRIVE_FRONT_RIGHT);
+  private final CANSparkMax driveBackLeft = new CANSparkMax(DriveConstants.DRIVE_BACK_LEFT, MotorType.kBrushed);
+  private final WPI_VictorSPX driveBackRight = new WPI_VictorSPX(DriveConstants.DRIVE_BACK_RIGHT);
+
+  private final MotorControllerGroup driveLeft = new MotorControllerGroup(driveFrontLeft, driveBackLeft);
+  private final MotorControllerGroup driveRight = new MotorControllerGroup(driveFrontRight, driveBackRight);
+  private final DifferentialDrive differentialDrive = new DifferentialDrive(driveLeft, driveRight);
+
+
+  public DriveSubsystem() {
+    driveFrontLeft.setNeutralMode(NeutralMode.Brake);
+    driveFrontRight.setNeutralMode(NeutralMode.Brake);
+    driveBackRight.setNeutralMode(NeutralMode.Brake);
+    driveBackLeft.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    driveRight.setInverted(true);
+  }
+
+  public void setMotors(double forwardSpeed, double turnSpeed)  {
+    differentialDrive.arcadeDrive(forwardSpeed, turnSpeed);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.PneumaticsConstants;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class IntakeSubsystem extends SubsystemBase {
+    
+    private final CANSparkMax intake = new CANSparkMax(IntakeConstants.INTAKE_MOTOR_ID, MotorType.kBrushless);
+
+    private final DoubleSolenoid intakePneumatic = new DoubleSolenoid(PneumaticsConstants.PCM,
+      PneumaticsModuleType.CTREPCM, IntakeConstants.INTAKE_FORWARD_PORT, IntakeConstants.INTAKE_REVERSE_PORT);
+    
+    public IntakeSubsystem() {
+
+    }
+
+    public void open() {
+        
+    }
+}
