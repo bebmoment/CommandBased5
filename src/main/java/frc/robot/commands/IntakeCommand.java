2550023bc009619b009739d522c9644328b93e1e
beb <benwangott@gmail.com>
2023-10-02 22:05:19 -0400

    intake command beginnings

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,11 +5,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+import java.util.function.DoubleSupplier;
+import frc.robot.Constants.IntakeConstants;;
 
 public class IntakeCommand extends CommandBase {
+ 
+  private final IntakeSubsystem m_intakeSubsystem;
+  private final DoubleSupplier speed;
+  
   /** Creates a new IntakeCommand. */
-  public IntakeCommand() {
+  public IntakeCommand(IntakeSubsystem intakeSubsystem, DoubleSupplier speedFunction) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_intakeSubsystem = intakeSubsystem;
+    speed = speedFunction;
+    addRequirements(this.m_intakeSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -18,11 +28,17 @@ public class IntakeCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_intakeSubsystem.setMotor(0);
+  }
 
   // Returns true when the command should end.
   @Override
