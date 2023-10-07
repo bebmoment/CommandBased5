84e8fdc3d588d6ce82ad6247bba96a14b957c8b2
beb <benwangott@gmail.com>
2023-10-06 21:24:15 -0400

    Added autonomous command and subsystems

--- src/main/java/frc/robot/subsystems/Stopwatch.java
+++ src/main/java/frc/robot/subsystems/Stopwatch.java
@@ -4,11 +4,32 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+/*
+ * Essentially a timer only ever used to autonomous for 2 seconds
+ * 
+ * CNE Bot 1 needed a way to keep track of time somewhere and so do I
+ * this is sufficient
+ */
 public class Stopwatch extends SubsystemBase {
+  private final Timer timer = new Timer();
   /** Creates a new Stopwatch. */
   public Stopwatch() {}
+
+  public void resetTimer() {
+    timer.reset();
+  }
+
+  public void startTimer() {
+    timer.start();
+  }
+
+  public double getTimer() {
+    return timer.get();
+  }
+
+
 
   @Override
   public void periodic() {
